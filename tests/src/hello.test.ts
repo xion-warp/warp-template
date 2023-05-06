38fa7e3f8553427c014ba4400764cf32ab73d4cc
Reyth3 <reyth@DESKTOP-A81EI9H.localdomain>
2023-05-06 13:35:44 +0200

    updated config; started porting over the tests

--- tests/src/hello.test.ts
+++ tests/src/hello.test.ts
@@ -1,30 +1,38 @@
 import { expect } from 'chai';
-import { SecretNetworkClient } from 'secretjs';
-import { getGenesisWallets, getLocalSecretConnection, storeAndInitContract } from './utils/localsecret'
+import { ArchwayClient, SigningArchwayClient } from '@archwayhq/arch3.js';
+import { getGenesisWallets, getConstantineConnection, storeAndInitContract, /*requestFaucetTokensConstantine*/ } from './utils/arch'
+import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
 
 describe('Hello World', () => {
     
-    let client: SecretNetworkClient;
+    let queryClient: ArchwayClient;
+    let signingClient: SigningArchwayClient;
+    let [a,b,c,d]: DirectSecp256k1HdWallet[] = Array(4);
     
-    before(() => {
-        client = getLocalSecretConnection();
+
+    before(async () => {
+        [a,b,c,d] = await getGenesisWallets();
+        [queryClient, signingClient] = await getConstantineConnection(a.mnemonic);
     })
 
-    it('Can get current block height', async () => {
-        const block = await client.query.tendermint.getLatestBlock({});
-        expect(block.block_id.hash).to.not.be.null;
+    it('Can get current chain id (is Constantine-2?)', async () => {
+        const chainId = await queryClient.getChainId();
+        expect(chainId).to.be.eq("constantine-2");
     });
 
-    it("Has correct account balance", async () => {
-        const [a, b, c, d] = getGenesisWallets();
-        let balance = await client.query.bank.balance({ address: a.address, denom: "uscrt" });
-        expect(parseInt(balance.balance.amount)).to.be.gte(99 * 1e6);
+    it("Genesis account has a balance bigger than 0UCONST", async () => {
+        let balance = await queryClient.getBalance(a.getAccounts()[0], "uconst");
+        expect(Number(balance.amount)).to.be.gt(0);
     });
+    /* This is not active due to fetch problems <--- FIXME:
+    it("equests 0.5CONST from the faucet", async () => {
+        let result = await requestFaucetTokensConstantine(a.getAccounts()[0], "500000uconst");
+        expect(result).to.be.eq(true);
+    });
+    */
     it("Correctly instantiates a contract", async() => {
-        let [a] = getGenesisWallets();
-        let signingClient = getLocalSecretConnection(a);
-        let address = await storeAndInitContract(signingClient, "./contract.wasm", {}, "New Contract "+Date.now());
-        console.log(address);
+        let address = await storeAndInitContract(signingClient, a.getAccounts()[0], "./contract.wasm", {}, "New Contract "+Date.now());
+        console.log("instantiated demo contract at: ", address);
         expect(address).to.be.not.eq("");
     });
 });
---
+++ tests/src/utils/arch.ts
@@ -0,0 +1,60 @@
+// FIXME: Port over to Archway once support for local test env & `warp node` is implemented
+
+import { MsgStoreCodeEncodeObject, ArchwayClient, SigningArchwayClient, SigningCosmWasmClient, SigningCosmWasmClientOptions,} from '@archwayhq/arch3.js'
+import { } from '@cosmjs/stargate';
+import * as fs from 'fs'
+import { Coin, DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
+import fetch from 'node-fetch';
+
+export async function getConstantineConnection(mnemonic?: string) : Promise<[ArchwayClient, SigningArchwayClient]> {
+  const network = {
+    chainId: 'constantine-2',
+    endpoint: 'https://rpc.constantine-2.archway.tech',
+    prefix: 'archway',
+  };
+  
+  let queryClient = await ArchwayClient.connect(network.endpoint);
+    
+  let signingClient: SigningArchwayClient;
+  if (mnemonic) {
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, { prefix: network.prefix });
+    let opt: SigningCosmWasmClientOptions = {};
+    
+    signingClient = await SigningArchwayClient.connectWithSigner(network.endpoint, wallet, {
+      ...opt,
+      prefix: network.prefix,
+    });
+  }
+    return [queryClient, signingClient];
+}
+
+export async function getGenesisWallets(): Promise<DirectSecp256k1HdWallet[]> {
+    let wallets: DirectSecp256k1HdWallet[] = new Array(4);
+    wallets[0] = await DirectSecp256k1HdWallet.fromMnemonic("grant rice replace explain federal release fix clever romance raise often wild taxi quarter soccer fiber love must tape steak together observe swap guitar");
+    wallets[1] = await DirectSecp256k1HdWallet.fromMnemonic("jelly shadow frog dirt dragon use armed praise universe win jungle close inmate rain oil canvas beauty pioneer chef soccer icon dizzy thunder meadow");
+    wallets[1] = await DirectSecp256k1HdWallet.fromMnemonic("chair love bleak wonder skirt permit say assist aunt credit roast size obtain minute throw sand usual age smart exact enough room shadow charge");
+    wallets[1] = await DirectSecp256k1HdWallet.fromMnemonic("word twist toast cloth movie predict advance crumble escape whale sail such angry muffin balcony keen move employ cook valve hurt glimpse breeze brick");
+    return wallets;
+}
+
+export async function storeAndInitContract(client: SigningArchwayClient, sender: string, contractPath: string, initMsg: any, label?: string, funds?: Coin[]): Promise<string> {
+    const bytecode = fs.readFileSync(contractPath) as Uint8Array;
+    let storeTx = await client.upload(sender, bytecode, "auto");
+    const codeId = storeTx.codeId;
+
+    const instantiate = await client.instantiate(sender, codeId, initMsg, label, "auto", { funds })
+
+    return instantiate.contractAddress;
+}
+
+/*
+export async function requestFaucetTokensConstantine(address: string, coin: string): Promise<boolean> {
+  let response = await fetch("https://faucet.constantine-2.archway.tech/", { method: "POST", body: {
+    address,
+    coins: [
+      coin
+    ]
+  }});
+  return response.ok;
+}
+*/
