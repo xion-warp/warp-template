9b74c417c906f8b4d6012c36cb184021ff602146
Reyth3 <mrreynevan2@gmail.com>
2023-05-06 15:15:53 +0200

    Fixed example tests

--- tests/src/hello.test.ts
+++ tests/src/hello.test.ts
@@ -1,37 +1,40 @@
 import { expect } from 'chai';
 import { ArchwayClient, SigningArchwayClient } from '@archwayhq/arch3.js';
-import { getGenesisWallets, getConstantineConnection, storeAndInitContract, /*requestFaucetTokensConstantine*/ } from './utils/arch'
+import { getGenesisWallets, getConstantineConnection, storeAndInitContract, requestFaucetTokensConstantine } from './utils/arch'
 import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
 
 describe('Hello World', () => {
     
     let queryClient: ArchwayClient;
     let signingClient: SigningArchwayClient;
-    let [a,b,c,d]: DirectSecp256k1HdWallet[] = Array(4);
-    
+    let a: DirectSecp256k1HdWallet;
+    let signerAddress: string;
 
     before(async () => {
-        [a,b,c,d] = await getGenesisWallets();
+        [a] = await getGenesisWallets();
         [queryClient, signingClient] = await getConstantineConnection(a.mnemonic);
+        signerAddress = (await a.getAccounts())[0].address;
     })
 
     it('Can get current chain id (is Constantine-2?)', async () => {
         const chainId = await queryClient.getChainId();
         expect(chainId).to.be.eq("constantine-2");
     });
+    it("Genesis account needs to have sufficient balance", async () => {
+        let balance = await queryClient.getBalance((await a.getAccounts())[0].address, "uconst");
+        if (Number(balance.amount) < 1000000) {
+            let addr = signerAddress;
+            console.log(addr);
+            let result = await requestFaucetTokensConstantine(addr, "500000uconst");
+            expect(result).to.be.eq(true);
+            balance = await queryClient.getBalance((await a.getAccounts())[0].address, "uconst");
+        }
+        expect(Number(balance.amount)).to.be.gte(1000000);
+    });
 
-    it("Genesis account has a balance bigger than 0UCONST", async () => {
-        let balance = await queryClient.getBalance(a.getAccounts()[0], "uconst");
-        expect(Number(balance.amount)).to.be.gt(0);
-    });
-    /* This is not active due to fetch problems <--- FIXME:
-    it("equests 0.5CONST from the faucet", async () => {
-        let result = await requestFaucetTokensConstantine(a.getAccounts()[0], "500000uconst");
-        expect(result).to.be.eq(true);
-    });
-    */
+
     it("Correctly instantiates a contract", async() => {
-        let address = await storeAndInitContract(signingClient, a.getAccounts()[0], "./contract.wasm", {}, "New Contract "+Date.now());
+        let address = await storeAndInitContract(signingClient, signerAddress, "./cool.wasm", { owner: signerAddress, message: "Hi!" }, "New Contract "+Date.now());
         console.log("instantiated demo contract at: ", address);
         expect(address).to.be.not.eq("");
     });
--- tests/src/utils/arch.ts
+++ tests/src/utils/arch.ts
@@ -1,10 +1,10 @@
 // FIXME: Port over to Archway once support for local test env & `warp node` is implemented
 
 import { MsgStoreCodeEncodeObject, ArchwayClient, SigningArchwayClient, SigningCosmWasmClient, SigningCosmWasmClientOptions,} from '@archwayhq/arch3.js'
-import { } from '@cosmjs/stargate';
+import { GasPrice } from '@cosmjs/stargate';
 import * as fs from 'fs'
 import { Coin, DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
-import fetch from 'node-fetch';
+import axios from 'axios';
 
 export async function getConstantineConnection(mnemonic?: string) : Promise<[ArchwayClient, SigningArchwayClient]> {
   const network = {
@@ -18,7 +18,7 @@ export async function getConstantineConnection(mnemonic?: string) : Promise<[Arc
   let signingClient: SigningArchwayClient;
   if (mnemonic) {
     const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, { prefix: network.prefix });
-    let opt: SigningCosmWasmClientOptions = {};
+    let opt: SigningCosmWasmClientOptions = { gasPrice: GasPrice.fromString("0.012uconst") };
     
     signingClient = await SigningArchwayClient.connectWithSigner(network.endpoint, wallet, {
       ...opt,
@@ -30,10 +30,10 @@ export async function getConstantineConnection(mnemonic?: string) : Promise<[Arc
 
 export async function getGenesisWallets(): Promise<DirectSecp256k1HdWallet[]> {
     let wallets: DirectSecp256k1HdWallet[] = new Array(4);
-    wallets[0] = await DirectSecp256k1HdWallet.fromMnemonic("grant rice replace explain federal release fix clever romance raise often wild taxi quarter soccer fiber love must tape steak together observe swap guitar");
-    wallets[1] = await DirectSecp256k1HdWallet.fromMnemonic("jelly shadow frog dirt dragon use armed praise universe win jungle close inmate rain oil canvas beauty pioneer chef soccer icon dizzy thunder meadow");
-    wallets[1] = await DirectSecp256k1HdWallet.fromMnemonic("chair love bleak wonder skirt permit say assist aunt credit roast size obtain minute throw sand usual age smart exact enough room shadow charge");
-    wallets[1] = await DirectSecp256k1HdWallet.fromMnemonic("word twist toast cloth movie predict advance crumble escape whale sail such angry muffin balcony keen move employ cook valve hurt glimpse breeze brick");
+    wallets[0] = await DirectSecp256k1HdWallet.fromMnemonic("grant rice replace explain federal release fix clever romance raise often wild taxi quarter soccer fiber love must tape steak together observe swap guitar", {prefix: "archway"});
+    wallets[1] = await DirectSecp256k1HdWallet.fromMnemonic("jelly shadow frog dirt dragon use armed praise universe win jungle close inmate rain oil canvas beauty pioneer chef soccer icon dizzy thunder meadow", {prefix: "archway"});
+    wallets[1] = await DirectSecp256k1HdWallet.fromMnemonic("chair love bleak wonder skirt permit say assist aunt credit roast size obtain minute throw sand usual age smart exact enough room shadow charge", {prefix: "archway"});
+    wallets[1] = await DirectSecp256k1HdWallet.fromMnemonic("word twist toast cloth movie predict advance crumble escape whale sail such angry muffin balcony keen move employ cook valve hurt glimpse breeze brick", {prefix: "archway"});
     return wallets;
 }
 
@@ -47,14 +47,13 @@ export async function storeAndInitContract(client: SigningArchwayClient, sender:
     return instantiate.contractAddress;
 }
 
-/*
+
 export async function requestFaucetTokensConstantine(address: string, coin: string): Promise<boolean> {
-  let response = await fetch("https://faucet.constantine-2.archway.tech/", { method: "POST", body: {
+  let response = await axios.post("https://faucet.constantine-2.archway.tech/", {
     address,
     coins: [
       coin
     ]
-  }});
-  return response.ok;
+  });
+  return response.status == 200;
 }
-*/
